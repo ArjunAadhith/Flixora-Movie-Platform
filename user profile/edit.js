f006948f8b4ba973a3b39bd76266e12492b23e31
Arjun Aadhith BS <arjunaadhith2004@gmail.com>
2025-03-03 23:34:42 +0530

    modify files

--- user profile/edit.js
+++ user profile/edit.js
@@ -1,16 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Select all profile cards that should be clickable
-    const profileCards = document.querySelectorAll(".profile-card:not(.add-profile)");
-
-    profileCards.forEach(card => {
-        card.addEventListener("click", function () {
-            // Get the profile name from `data-profile` attribute
-            const profileName = this.getAttribute("data-profile");
-            
-            if (profileName) {
-                // Redirect to set-profile.html with profile name as query parameter
-                window.location.href = `user-profile/set-profile.html?profile=${encodeURIComponent(profileName)}`;
-            }
-        });
-    });
-});
+function redirectToProfile() {
+    window.location.href = "set-profile.html";
+}
